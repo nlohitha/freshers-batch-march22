2fdbf73e83530b048b272af92d05b8000ce87dae
nlohitha <102664791+nlohitha@users.noreply.github.com>
2022-04-16 13:55:18 +0530

    Delete assignment16.sh

--- assignment16.sh
+++
@@ -1,8 +0,0 @@
-
-#!/bin/bash
-# simple file reading (non-binary) and displaying one line at a time
-echo "Enter a filename to read: "
-read FILE
-while read -r SUPERHERO; do
-echo "Superhero Name: $SUPERHERO"
-done < "$FILE"
