fec7950ce50b7624f06aafeb5918756b5662a077
nlohitha <102664791+nlohitha@users.noreply.github.com>
2022-04-16 13:47:22 +0530

    Add files via upload

---
+++ shellassignment/shell scripting/python/Logical/Assignment-01.py
@@ -0,0 +1,31 @@
+list1 = ["Lohitha" , "Bujji", "punitha", "Kavya"]
+
+list2 = []
+
+
+
+print(len(list1))
+
+
+
+#abs()
+
+num = -24
+
+print(abs(num))
+
+
+
+#round
+
+
+
+x = round(4.2)
+
+print(x)
+
+
+
+y = round(3.6)
+
+print(y)
---
+++ shellassignment/shell scripting/python/automation/assign2.py
@@ -0,0 +1,20 @@
+import re
+
+def regex_strip(s, char=None):
+
+    if not char:
+        strip_left = re.compile(r'^\s*') #string starting with whitespace
+        strip_right = re.compile(r'\s*$') #string ending with whitespace
+
+        s = re.sub(strip_left, "", s) #replacing strip_left with "" in string s
+        s = re.sub(strip_right, "", s) #replacing strip_right with "" in string s
+
+    else:
+        strip_char = re.compile(char)
+        s = re.sub(strip_char, "", s)
+    return s
+
+if __name__ == '__main__':
+    string_to_be_stripped = input("Enter string to be stripped: ")
+    char_to_be_removed = input("Enter character to be removed, if none press enter: ")
+    print(regex_strip(string_to_be_stripped, char_to_be_removed))
