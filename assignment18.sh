e265e048344001ab09ef94d6e7c3dd8678ebfb13
nlohitha <102664791+nlohitha@users.noreply.github.com>
2022-04-16 13:58:02 +0530

    Delete assignment18.sh

--- assignment18.sh
+++
@@ -1,8 +0,0 @@
-echo "Enter filename to parse:" read FILE
-echo "Enter the Delimiter:"read DELIM
-IFS="$DELIM"
-while read -r CPU MEMORY DISK;do
-echo "CPU:$CPU"
-echo "Memory:$MEMORY"
-echo "Disk:$DISK"
-done<"$FILE">
